00bf3d9e924a5c722945d24462e601c4c06ecdcf
Rafael William <rafael.fim@hotmail.com>
2023-07-18 15:38:26 -0300

    components: sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Header } from './components/Header'
 
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/SideBar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main></main>
+      </div>
     </div>
   )
 }

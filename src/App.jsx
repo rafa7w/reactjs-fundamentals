b6bc3f2dec78ad136d45759757a19a768017c9ff
Rafael William <rafael.fim@hotmail.com>
2023-07-21 14:27:40 -0300

    iterable posts

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,52 @@ import { Post } from './components/Post'
 import styles from './App.module.css'
 import './global.css'
 
+/* 
+Informações que um Post precisa:
+author: {avatar_url: "", name: "", role: ""}
+publishedAt: Date
+content: String
+*/
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/rafa7w.png',
+      name: 'Rafael William',
+      role: 'TI Assistent'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-07-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'
+      },
+      { type: 'link', content: 'jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-07-20 20:00:00')
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -13,7 +59,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
